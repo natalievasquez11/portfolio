7bc6c816f2f06d916e0ff880487d879603fb79f3
Natalie Vasquez <natalievasquez@natalies-air-3.lan>
2019-04-29 20:46:18 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './css/App.css';
 import Main from "./components/Main/Main";
+import Links from "./components/Links/Links";
 
 function App() {
   return (
     <div className="App">
       < Main />
-      <div className="links">
-      <a href="https://www.linkedin.com/in/natalie-vasquez-csm-b0a59749/" target="_blank"><i className="fab fa-linkedin-in fa-3x"></i></a>
-      <a href="https://github.com/natalievasquez11" target="_blank"><i className="fab fa-github-alt fa-3x"></i></a>
-      <a href="#"><i className="fas fa-paper-plane fa-3x"></i></a>
-      </div>
+      < Links />
     </div>
   );
 }
---
+++ src/components/Links/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Links() {
+    return (
+        <div>
+            <div className="links">
+                <a href="https://www.linkedin.com/in/natalie-vasquez-csm-b0a59749/" target="_blank"><i className="fab fa-linkedin-in fa-3x"></i></a>
+                <a href="https://github.com/natalievasquez11" target="_blank"><i className="fab fa-github-alt fa-3x"></i></a>
+                <a href="mailto:natalievasquez11@gmail.com"><i className="fas fa-paper-plane fa-3x"></i></a>
+            </div>
+        </div>
+    )
+}
+
+export default Links;
