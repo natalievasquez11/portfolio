16201d5631c5bec6d7256f584d5f2cc2bf54106f
Natalie Vasquez <natalievasquez@natalies-air-3.lan>
2019-04-29 20:53:42 -0500

    Updates

---
+++ precache-manifest.34c9e82cf54317ca8cdb9e38a2244886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b0d9d02e0a718be849abf12bd961105",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1e7514f27f74eabe70eb",
+    "url": "/portfolio/static/css/main.229fa282.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1e7514f27f74eabe70eb",
+    "url": "/portfolio/static/js/main.6bcf6fb3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
